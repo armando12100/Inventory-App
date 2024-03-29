99e60eaeb2d819218fc6bf864bec80bcda2d26bf
Armando Sanchez <asan5900@gmail.com>
2024-03-29 15:36:46 -0700

    Youtube Links added

--- client/client/src/components/DeckDetails.jsx
+++ client/client/src/components/DeckDetails.jsx
@@ -8,14 +8,16 @@ const DeckDetails = () => {
   console.log(data)
 
   const opts = {
-    height: '500',
-    width: '95%'
+    height: '450',
+    width: '80%'
   }
   return (
     <>
-      <h1 className="font-bold text-2xl pt-2 pl-3 text-center">{data.deckTitle}</h1>
-      <Youtube videoId="Y5k3qiO17nc" opts={opts} className="flex justify-center bg-indigo-950" />
-      <img src={data.image} alt="" className="object-fill w-full lg:h-100 pt-2" />
+      <h1 className="font-bold text-2xl pt-2 pb-2 pl-3 text-center
+       bg-indigo-950 text-white mt-1">{data.deckTitle}</h1>
+      <Youtube videoId={data.youtube} opts={opts} className="flex justify-center bg-indigo-900
+      border-4 border-solid border-white rounded-lg p-2" />
+      <img src={data.image} alt="" className="object-fill w-full lg:h-100 pt-1" />
       <p className="py-3 pl-3 font-bold">{data.description}</p>
       <p className="py-3 pl-3 font-bold">{data.description}</p>
       <p className="py-3 pl-3 font-bold">{data.description}</p>
--- client/client/src/data/MetaDeckCardData.jsx
+++ client/client/src/data/MetaDeckCardData.jsx
@@ -20,6 +20,7 @@ export default [
         pageId: 1,
         id: 1,
         code: "https://dreamborn.ink/decks/MoK12DW0GesLM60afoQD",
+        youtube: "Y5k3qiO17nc",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -28,6 +29,8 @@ export default [
         image: Sapphire_Steel,
         pageId: 2,
         id: 2,
+        code:"",
+        youtube: "lbdt1KshaMQ",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -36,6 +39,8 @@ export default [
         image: Ruby_Sapphire,
         pageId: 3,
         id: 3,
+        code:"",
+        youtube:"OMAoxvmKlzA",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -45,6 +50,7 @@ export default [
         id: 4,
         pageId: 4,
         code: "https://dreamborn.ink/decks/sDSWJoWS1ywsa7SKanju",
+        youtube: "MfXWqHQwqUQ",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -58,6 +64,7 @@ export default [
         id: 6,
         pageId: 6,
         code: "https://dreamborn.ink/decks/QUFGSTdQtWRoMFiJWe4T",
+        youtube: "af0n1jAx358",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -67,6 +74,7 @@ export default [
         id: 7,
         pageId: 7,
         code: "https://dreamborn.ink/decks/kXuJIy4vdP6EBcpQZrQA",
+        youtube: "5x2zoWUMTCw",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -89,6 +97,7 @@ export default [
         id: 10,
         pageId: 10,
         code: "https://dreamborn.ink/decks/hwp9mf79XCqZbO1OKDma",
+        youtube: "t4-pNzZrmbo",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -98,6 +107,7 @@ export default [
         id: 11,
         pageId: 11,
         code: "https://dreamborn.ink/decks/MgcDbTYCBsQ0lA6IGGHF",
+        youtube: "bzr9MGPT7FM",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     },
@@ -107,6 +117,7 @@ export default [
         id: 12,
         pageId: 12,
         code: "https://dreamborn.ink/decks/KuqTTv7wBzQUgaM2atWE",
+        youtube: "ZRXqMxhwt_g",
         description: 
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, consequuntur quis omnis, quidem ipsum repellat corrupti fuga earum vero numquam, ipsam perspiciatis possimus. Dolore minima incidunt assumenda eos quasi omnis."
     }
